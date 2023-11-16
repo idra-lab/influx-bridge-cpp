13072ea8ac3655bffdd0af4058ce69535fd0a77f
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-11-16 16:58:31 +0100

    Fixed .gitignore Removed defines.hpp that's configured by cmake Fixed .gitignore that was excluding cmake lists from staging

--- include/influxdb_bridge_cpp/defines.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef INFLUXDBBRIDGE_DEFINES_HPP__
-#define INFLUXDBBRIDGE_DEFINES_HPP__
-
-#define PRJ_ROOT_DIR "/home/matteo/workspaces/ros/src/influxdb-bridge-cpp"
-
-#endif  // INFLUXDBBRIDGE_DEFINES_HPP__
